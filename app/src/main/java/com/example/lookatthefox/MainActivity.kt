c8db65849583eadaa5e896bc71eb6b418b81682d
InnaTsvetkova <mngr.tsvetkova@gmail.com>
2022-12-10 15:56:10 +0700

    Added activity and image loading

--- app/src/main/java/com/example/lookatthefox/MainActivity.kt
+++ app/src/main/java/com/example/lookatthefox/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.lookatthefox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.load
+import com.example.lookatthefox.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val randomId = Random.nextInt(1,121)
+            val uri = "https://randomfox.ca/images/$randomId.jpg"
+            binding.image.load(uri)
+        }
     }
 }
